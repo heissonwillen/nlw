375307a232749bc4f7c91b6dabc4a3abd435c30b
heisson <heisson70@gmail.com>
2020-06-03 12:13:17 -0300

    Added points migration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,42 +1,12 @@
 import express from 'express';
 // import * as express from 'express'
+import routes from './routes'
 
 const app = express();
 
 app.use(express.json());
 
-const users = [
-    "Heisson",
-    "Willen",
-    "Esteves",
-    "Lima"
-]
+app.use(routes)
 
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) =>{
-    const id = Number(request.params.id);
-    const user = users[id];
-
-    return response.json(user)
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    console.log(data);
-
-    const user = {
-        name: data.name,
-        email: data.email
-    };
-
-    return response.json(user);
-});
 
 app.listen(3333)
