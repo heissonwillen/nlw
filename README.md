f3bc44007baad2d86ee31dc8d6392c45a38ac21e
heisson <heisson70@gmail.com>
2020-06-03 11:31:51 -0300

    Added post routes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# nlw
+# Next Level Week by Rocketseat
+
+Application developed during the first next level week by Rocketseat.
+This is a both phone and web app that helps people to find garbage collection areas
+nearby them.
+
+It is going to be build with React and NodeJS.
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,25 +3,40 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
 
-app.get('/users', (request, response) => {
-  console.log("Users listing");
-
-  response.json([
-    "Hisson",
+const users = [
+    "Heisson",
     "Willen",
     "Esteves",
     "Lima"
-  ]);
+]
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) =>{
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    return response.json(user)
 });
 
 app.post('/users', (request, response) => {
-  const user = {
-    name: "Heisson",
-    email: "heisson@heisson.com"
-  };
+    const data = request.body;
 
-  return response.json(user);
+    console.log(data);
+
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333)
