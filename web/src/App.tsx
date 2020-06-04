e2b20f6e139c6fa8939d2d71a53ab57827712483
heisson <heisson70@gmail.com>
2020-06-04 10:15:39 -0300

    Added css and first two pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
 
-import Header from './Header';
+import Routes from './routes';
 
 function App() {
-    const [counter, setCounter] = useState(0);
-
-    function handleButtonClick() {
-        setCounter(counter + 1);
-        console.log(counter)
-    }
-
     return (
-
         <div>
-            <Header title={`Contador: ${counter}`} />
-
-            <h1>{counter}</h1>
-
-            <button type="button" onClick={handleButtonClick}>Increase</button>
+            <Routes />
         </div>
     );
 }
