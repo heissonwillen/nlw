669f825246a7c232493991af0b449aee2a3da33a
heisson <heisson70@gmail.com>
2020-06-04 11:17:30 -0300

    Added maps feature

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,8 +1,126 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+
+import api from '../../services/api';
+import './styles.css';
+import logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
+    useEffect(() => {
+        api.get('items').then(response => {
+            console.log(response);
+        });
+    }, []);
+
+
     return (
-        <h1>Hello again.</h1>
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"></img>
+
+                <Link to="/">
+                    <FiArrowLeft />
+                    Voltar para a home
+                </Link>
+            </header>
+
+            <form>
+                <h1>Cadastro do <br /> ponto de coleta</h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+
+                    <div className="field">
+                        <label htmlFor="name" className="name">Nome da entidade</label>
+                        <input type="text" name="name" id="name"/>
+                    </div>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email" className="field">E-mail</label>
+                            <input type="email" name="email" id="email"/>
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="whatsapp" className="name">WhatsApp</label>
+                            <input type="text" name="whatsapp" id="whatsapp"/>
+                        </div>
+                    </div>
+
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o Endereço no mapa</span>
+                    </legend>
+
+                    <Map center={[-4.8773199, -43.37]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-4.8773199, -43.37]}/>
+                    </Map>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione uma UF</option>
+                            </select>
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidate</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Itens de coleta</h2>
+                        <span>Selecione um ou mais itens abai</span>
+                    </legend>
+
+                    <ul className="items-grid">
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li className="selected">
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                    </ul>
+                </fieldset>
+
+                <button type="submit">Cadastrar ponto de coleta</button>
+            </form>
+        </div>
     );
 }
 
