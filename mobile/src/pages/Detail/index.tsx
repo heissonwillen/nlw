18bfd15e4f3d87fb124b3b650eb3aa8cc44cd356
heisson <heisson70@gmail.com>
2020-06-06 00:53:59 -0300

    Added searching for cities and states

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Constants from 'expo-constants'
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { View, Text, Image, StyleSheet, TouchableOpacity, SafeAreaView, Fragment } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, SafeAreaView, Fragment, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import api from '../../services/api';
+import * as MailComposer from 'expo-mail-composer';
 
 interface Params {
     point_id: number;
@@ -44,6 +45,17 @@ const Detail = () => {
         navigation.goBack();
     }
 
+    function handleComposeMail() {
+        MailComposer.composeAsync({
+            subject: 'Interesse na coleta de resíduos',
+            recipients: [data.point.email],
+        })
+    }
+
+    function handleWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse sobre coleta de resíduos.`)
+    }
+
     if (!data.point) {
         return null;
     }
@@ -68,12 +80,12 @@ const Detail = () => {
             </View>
 
             <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={() => {}}>
+                <RectButton style={styles.button} onPress={handleWhatsapp}>
                     <FontAwesome name="whatsapp" size={20} color="#FFF"/>
                     <Text style={styles.buttonText}>WhatsAspp</Text>
                 </RectButton>
 
-                <RectButton style={styles.button} onPress={() => {}}>
+                <RectButton style={styles.button} onPress={handleComposeMail}>
                     <Icon name="mail" size={20} color="#FFF"/>
                     <Text style={styles.buttonText}>E-mail</Text>
                 </RectButton>
